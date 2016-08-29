2e4055cc576f37a8f8c6abc4361bafb1acc18a9c
Markus Altmeyer <maltmeyer@avarteq.de>
2016-08-29 11:04:44 +0200

    add config for api roles

--- lib/omniauth-keycloak/config.rb
+++ lib/omniauth-keycloak/config.rb
@@ -1,8 +1,12 @@
 class OmniauthKeycloak::Config
-    attr_accessor :allowed_realm_roles, :allowed_client_roles, :token_cache_expires_in
+    attr_accessor :allowed_realm_roles, :allowed_client_roles, :token_cache_expires_in, :login_redirect_url, :allowed_realm_roles_api, :allowed_client_roles_api
+
     def initialize
-      @allowed_realm_roles = ["admin"]
-      @allowed_client_roles = ["admin"]
+      @allowed_realm_roles = []
+      @allowed_client_roles = []
+      @allowed_realm_roles_api = []
+      @allowed_client_roles_api = []
+
       @token_cache_expires_in = 10.minutes
     end
 end
