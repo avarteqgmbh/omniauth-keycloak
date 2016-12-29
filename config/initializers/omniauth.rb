7ee7f398484f4fc7390f0b19e10fe3da3d5e40cc
Matthias Folz <folz@avarteq.de>
2016-12-29 14:43:37 +0100

    remove not neccessary files

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.delete OmniAuth::Builder
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-provider :keycloak, ENV["keycloak_client_id"], ENV["keycloak_client_secret"], scope: "openid", public_key: ENV["keycloak_public_key"],client_options: {:site => ENV["keycloak_url"], :authorize_url => ENV["keycloak_authorize_url"], :token_url => ENV["keycloak_token_endpoint"]}
-OmniAuth.config.on_failure = Proc.new do |env|
-  OmniauthKeycloak::CallbackController.action(:omniauth_error_callback).call(env)
-  #this will invoke the omniauth_error_callback action in ApplicationController.
-end
-end
--- lib/omniauth-keycloak/engine.rb
+++
@@ -1,5 +0,0 @@
-module OmniauthKeycloak
-  class Engine < ::Rails::Engine
-     #isolate_namespace OmniauthKeycloak
-  end
-end
