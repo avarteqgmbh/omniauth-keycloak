a3a0c7d3d990d84fba2d6cebeb74e6b7653bdb29
Tim Meier <raco0n@gmx.de>
2017-08-30 11:06:28 +0000

    Fix rack middleware

--- lib/omniauth_keycloak/rack.rb
+++ lib/omniauth_keycloak/rack.rb
@@ -2,13 +2,24 @@ class OmniauthKeycloak::Rack < Rack::Auth::AbstractHandler
   include OmniauthKeycloak::SharedControllerMethods
 
   def call(env)
-    token = get_token(env.request)
+    request = { headers: env }
+    token = get_token(OpenStruct.new(request))
 
     if token and check_client_roles_api(token) and check_realm_roles_api(token)
       return @app.call(env)
     else
       unauthorized
     end
+  end
+
+  private
+
+  def challenge
+    'Bearer realm="%s"' % realm
+  end
+
+  def valid?(auth)
+    @authenticator.call(*auth.credentials)
   end
 
 end
