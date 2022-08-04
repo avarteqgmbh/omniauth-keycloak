18ad777640caa3ec30a0b880863a24e42cd4cc1c
smenz-any <56024716+smenz-any@users.noreply.github.com>
2022-08-04 06:39:38 +0200

    BUM-7600 Remove: autoloading routes (#9)
    * BUM-7600 Remove: autoloading routes
    Update: uri.join call tto avoid overriding endpoints
    Update: controller eager load path from engine
    
    * BUM-7600 Add: server prefix config
    
    Co-authored-by: Daniel H. <dhorbach148@users.noreply.github.com>
    
    * BUM-7600 Add: possibility to load mutiple provieders
    Update: configuration to allow to use a prefix
    Update: token and controller to use custom config
    
    * BUM-7660 Remove: unused default class
    
    * BUM-7803 Update: controller helper method current_user renamed
    
    This caused a conflict with the bum_core method.
    
    Co-authored-by: Daniel H. <dhorbach148@users.noreply.github.com>
    
    * BUM-7600 Update: ruby version to 2.6.7
    
    Co-authored-by: Daniel H. <dhorbach148@users.noreply.github.com>

--- lib/omniauth_keycloak/omniauth_controller_extension.rb
+++ lib/omniauth_keycloak/omniauth_controller_extension.rb
@@ -12,10 +12,10 @@ module OmniauthKeycloak::OmniauthControllerExtension
       end
     end
 
-    def login(keycloak_token,refresh_token = nil)
+    def login(keycloak_token,refresh_token = nil, token_cache_expires = OmniauthKeycloak.config.token_cache_expires_in.minutes)
       keycloak_token.refresh_token = refresh_token if refresh_token
       OmniauthKeycloak.log('Remember token')
-      Rails.cache.write(keycloak_token.sub,keycloak_token,:expires_in => OmniauthKeycloak.config.token_cache_expires_in.minutes)
+      Rails.cache.write(keycloak_token.sub,keycloak_token,:expires_in => token_cache_expires)
       session[:omniauth_keycloak_sub] = keycloak_token.sub
       
     end
@@ -25,26 +25,26 @@ module OmniauthKeycloak::OmniauthControllerExtension
       render text: "Logout successfull"
     end
 
-    def logout_keycloak
+    def logout_keycloak(realm_url = OmniauthKeycloak.config.realm_url, logout_redirect_url = OmniauthKeycloak.config.logout_redirect_url)
       clear_session
 
-      url = OmniauthKeycloak.config.realm_url +  "/protocol/openid-connect/logout"
-      if OmniauthKeycloak.config.logout_redirect_url
-        url += "?redirect_uri=#{OmniauthKeycloak.config.logout_redirect_url}"
+      url = realm_url +  "/protocol/openid-connect/logout"
+      if logout_redirect_url
+        url += "?redirect_uri=#{logout_redirect_url}"
       else
         url += "?redirect_uri=#{request.base_url}"
       end
       redirect_to url
     end
 
-    def check_client_roles(token)
+    def check_client_roles(token, allowed_client_roles = OmniauthKeycloak.config.allowed_client_roles)
       OmniauthKeycloak.log("User client_roles: #{token.client_roles}")
-      (token.client_roles & OmniauthKeycloak.config.allowed_client_roles).count > 0
+      (token.client_roles & allowed_client_roles).count > 0
     end
 
-    def check_realm_roles(token)
+    def check_realm_roles(token, allowed_realm_roles = OmniauthKeycloak.config.allowed_realm_roles)
       OmniauthKeycloak.log("User realm_roles: #{token.realm_roles}")
-      (token.realm_roles & OmniauthKeycloak.config.allowed_realm_roles).count > 0
+      (token.realm_roles & allowed_realm_roles).count > 0
     end
 
   end
