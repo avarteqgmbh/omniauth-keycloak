12310b15a2195e3bcbf45b4c069de83a0d2e157a
Dyu Vu <dvu@avarteq.de>
2023-08-29 09:32:30 +0200

    Add after do to unset env for keycloak server prefix

--- spec/lib/omniauth_keycloak/configuration_spec.rb
+++ spec/lib/omniauth_keycloak/configuration_spec.rb
@@ -4,25 +4,24 @@ describe 'OmniauthKeycloak::Configuration' do
 
   describe 'Keycloak url detection' do
     context 'without auth/ in url' do
-      before do
-        ENV["keycloak_url"] = 'http://auth-test.anynines.com/'
-      end
-
       it 'should return correct url without auth/ in url' do
-        expect(OmniauthKeycloak::Configuration.new.realm_url).to eq('http://auth-test.anynines.com/realms/')
+        expect(OmniauthKeycloak::Configuration.new.realm_url).to eq('http://localhost-test.de/realms/')
       end
     end 
 
     context 'with auth/ in url' do
       before do
-        ENV["keycloak_url"] = 'http://auth-test.anynines.com/'
         ENV['keycloak_server_prefix'] ='auth'
       end
 
-      it 'should return correct url with auth/ in url' do
-        expect(OmniauthKeycloak::Configuration.new.realm_url).to eq('http://auth-test.anynines.com/auth/realms/')
+      after do
+        ENV.delete('keycloak_server_prefix')
       end
-    end 
+
+      it 'should return correct url with auth/ in url' do
+        expect(OmniauthKeycloak::Configuration.new.realm_url).to eq('http://localhost-test.de/auth/realms/')
+      end
+    end
   end
 
 end
